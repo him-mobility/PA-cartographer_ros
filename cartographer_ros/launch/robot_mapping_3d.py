ee95ded25c24bec2e336ad3d39a6af5a206370a0
mia2583 <mia2583@pusan.ac.kr>
2026-04-29 17:20:27 +0900

    [feat] 단일 라이다 지원 - 하나의 라이다 토픽만을 구독하여 3D SLAM을 실행할 수 있는 독립 런치 파일 구현 - 다양한 로봇 모델 지원을 위해 URDF 경로를 런치 인자로 전달받도록 개선 - 기본 설정 파일 robot_3d.lua 추가

---
+++ cartographer_ros/launch/robot_mapping_3d.py
@@ -0,0 +1,118 @@
+"""
+  Copyright 2018 The Cartographer Authors
+  Copyright 2022 Wyca Robotics (for the ros2 conversion)
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+"""
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node, SetRemap
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+
+def generate_launch_description():
+
+    ## ***** Launch arguments *****
+    use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value = 'False')
+    cartographer_config_dir_arg = DeclareLaunchArgument(
+        'cartographer_config_dir',
+        default_value = os.path.join(FindPackageShare('cartographer_ros').find('cartographer_ros'), 'configuration_files'),
+        description = 'Full path to config directory')
+    configuration_basename_arg = DeclareLaunchArgument(
+        'configuration_basename',
+        default_value = 'robot_3d.lua',
+        description = 'Name of the lua configuration file')
+    load_state_filename_arg = DeclareLaunchArgument(
+        'load_state_filename',
+        default_value = '',
+        description = 'Full path to a pbstream file to load a saved state.')
+    urdf_arg = DeclareLaunchArgument(
+        'urdf',
+        default_value='MRO_ROVER.urdf',
+        description='Name of the URDF file in the urdf directory'
+    )
+
+    pkg_share = FindPackageShare('cartographer_ros').find('cartographer_ros')
+    urdf_file_path = PathJoinSubstitution([
+        pkg_share,
+        'urdf',
+        LaunchConfiguration('urdf')
+    ])
+    with open(urdf_file_path, 'r') as infp:
+        robot_desc = infp.read()
+
+    ## Robot State Publisher
+    robot_state_publisher_node = Node(
+        package = 'robot_state_publisher',
+        executable = 'robot_state_publisher',
+        parameters=[
+            {'robot_description': robot_desc},
+            {'use_sim_time': LaunchConfiguration('use_sim_time')}],
+        output = 'screen'
+        )
+    
+    # Joint State Publisher
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}],
+        output='screen'
+    )
+
+    # Static TF for base_footprint
+    static_baselink_publisher_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=['0', '0', '-0.06', '0', '0', '0', 'base_link', 'base_footprint'],
+        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+    )
+
+    cartographer_node = Node(
+        package = 'cartographer_ros',
+        executable = 'cartographer_node',
+        parameters = [{'use_sim_time': LaunchConfiguration('use_sim_time')}],
+        arguments = [
+            '-configuration_directory', LaunchConfiguration('cartographer_config_dir'),
+            '-configuration_basename', LaunchConfiguration('configuration_basename'),
+            '-load_state_filename', LaunchConfiguration('load_state_filename'),
+            '--ros-args',
+            '--remap', 'points2:=/point_cloud',
+            '--remap', 'imu:=/imu_data'],
+        output = 'screen'
+        )
+
+    cartographer_occupancy_grid_node = Node(
+        package = 'cartographer_ros',
+        executable = 'cartographer_occupancy_grid_node',
+        parameters = [
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+            {'resolution': 0.05}],
+        )
+
+    return LaunchDescription([
+        use_sim_time_arg,
+        cartographer_config_dir_arg,
+        configuration_basename_arg,
+        load_state_filename_arg,
+        urdf_arg, 
+        # Nodes
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        static_baselink_publisher_node,
+        cartographer_node,
+        cartographer_occupancy_grid_node,
+    ])
